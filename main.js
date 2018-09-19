8e3ee0de9617bd71cf2f219adaca3e8756ebe5ea
Adriean Khisbe <adriean.khisbe@live.fr>
2018-09-19 15:12:30 +0200

    Prototyping things forward

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const _ = require('lodash');
+const {Transform} = require('stream') 
 const FetchPaginatedStream = require('./src/fetch-paginated-stream');
 
-const numberFetcher = async ({limit = 12, offset= 0}, cb) => {
+const numberFetcher = async ({ limit = 12, offset = 0 }, cb) => {
     return _.range(offset, offset + limit);
 };
 
 
-const readable = new FetchPaginatedStream({fetcher: numberFetcher});
-readable.pipe(process.stdout);
+const readable = new FetchPaginatedStream({ fetcher: numberFetcher });
+readable
+    .pipe(new Transform({
+        writableObjectMode: true,
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.toString());
+        }
+    }))
+    .pipe(process.stdout);
--- src/fetch-paginated-stream.js
+++ src/fetch-paginated-stream.js
@@ -6,14 +6,22 @@ class FetchPaginatedStream extends Readable {
     super(_.extend({objectMode: true}, options));
     this.fetcher = options.fetcher;
     this.position = 0;
+    this.buffer = [];
   }
 
+  _pushResult(res){
+    if(_.isEmpty(res)) return null;
+    const shouldContinue = this.push(_.head(res));
+    if (shouldContinue) return this._pushResult(_.tail(res));
+    console.log('STOP')
+    return _.tail(res);
+  }
+  
   _read(size) {
+    // _read() should continue reading from the resource and pushing data until readable.push() returns false
     this.fetcher({offset: 0}).then(res => {
-        this.push('yo')
-        this.push({helo: 'yo'})
-        _.map(res, v => this.push(v));
-        this.push(null)
+      const remainingElements = this._pushResult(res);
+      console.log('REMAIN', remainingElements);
     }) ;
   }
 }
