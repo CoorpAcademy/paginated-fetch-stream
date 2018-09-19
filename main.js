13d564d913d063cabcc360d45a8ca1f5cfa6c914
Adriean Khisbe <adriean.khisbe@live.fr>
2018-09-19 16:31:43 +0200

    A push A Read keep the big bug away

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ const {Transform} = require('stream')
 const FetchPaginatedStream = require('./src/fetch-paginated-stream');
 
 const numberFetcher = async ({ limit = 12, offset = 0 }, cb) => {
-    console.log('offset', offset, _.range(offset, offset + limit))
     return _.range(offset, offset + limit);
 };
 
 
 const readable = new FetchPaginatedStream({ fetcher: numberFetcher });
 readable.on('data', value => {
-    console.log('>>>', value, readable.position, readable.readCalled, readable.buffer)
+    console.log('>>>', value)
     if ((value + 1) % 20 == 0) {
         console.log('<<<<<<<<<<<<<<<<PAUSE>>>>>>>>>>>>>>>>>>>>><')
         readable.pause();
--- src/fetch-paginated-stream.js
+++ src/fetch-paginated-stream.js
@@ -3,7 +3,7 @@ const _ = require('lodash');
 
 class FetchPaginatedStream extends Readable {
   constructor(options) {
-    super(_.extend({objectMode: true}, options));
+    super(_.extend({ objectMode: true }, options));
     this.fetcher = options.fetcher;
     this.position = 0;
     this.pageSize = 16
@@ -11,44 +11,24 @@ class FetchPaginatedStream extends Readable {
     this.readCalled = false;
   }
 
-  _pushResult(res){
-    console.log(this.buffer)
-    if(_.isEmpty(res)) return null;
-    const shouldContinue = this.push(_.head(res));
-    //console.log('shouldContinue', shouldContinue)
-    if (shouldContinue) return this._pushResult(_.tail(res));
-    console.log('STOP')
-    return _.tail(res);
-  }
-
   async _fetch() {
     // console.log('FETCH', offset)
-    this.fetcher({offset: this.position }).then(res => {
+    this.fetcher({ offset: this.position }).then(res => {
       this.position += _.size(res);
-      const remainingElements = this._pushResult(res);
-      //console.log('remaining meleme', remainingElements)
-      console.log(this.buffer, remainingElements)
-      if(_.isEmpty(remainingElements)) {
-        //console.log('recursive call to fetch')
-        return this._fetch()
-      }
-      this.buffer = remainingElements;
+      this.buffer = _.concat(this.buffer, res);
     });
   }
-  
+
   _read(size) {
-    console.log('REEEEEAAAAAD')
-    if (this.readCalled) return;
     if (!_.isEmpty(this.buffer)) {
-      console.log('RETRIEVING FROM BUFFER')
-      const remainingElements = this._pushResult(this.buffer);
-      this.buffer = remainingElements;
-      if (_.isEmpty(this.buffer))
-        this._fetch().then(() => { this.readCalled = false; })
+      this.push(_.head(this.buffer));
+      this.buffer = _.tail(this.buffer);
     } else {
-      this._fetch().then(() => { this.readCalled = false; })
+      this._fetch().then(() => {
+        this.push(_.head(this.buffer));
+        this.buffer = _.tail(this.buffer);
+      })
     }
-    this.readCalled = true;
   }
 }
 
